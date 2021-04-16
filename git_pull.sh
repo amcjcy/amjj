6e499a08735fdd85eba3dbf601905145f507ab7d
amcjcy <82363754+amcjcy@users.noreply.github.com>
2021-04-16 19:21:19 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -19,7 +19,7 @@ ContentVersion=${ShellDir}/version
 ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
-ScriptsURL=https://gitee.com/amcjcy/amr
+ScriptsURL=https://github.com/amcjcy/amr
 ShellURL=https://github.com/amcjcy/amjj
 
 
