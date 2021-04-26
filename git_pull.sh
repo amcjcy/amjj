995ab50ef90d467e4c1ab623116fc70e61cc9ac9
amcjcy <82363754+amcjcy@users.noreply.github.com>
2021-04-26 17:22:24 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -19,7 +19,7 @@ ContentVersion=${ShellDir}/version
 ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
-ScriptsURL=https://github.com/RikudouPatrickstar/jd_scripts
+ScriptsURL=https://github.com/gossh520/jd_scripts
 ShellURL=https://github.com/amcjcy/amjj
 
 
